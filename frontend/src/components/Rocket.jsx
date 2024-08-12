d7bb9aaa74b0a863b36e63712de6f9f21530057c
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-08-11 22:30:26 -0400

    Added a separate section for complete configurations to allow connecting to the backend

--- frontend/src/components/Rocket.jsx
+++ frontend/src/components/Rocket.jsx
@@ -2,19 +2,19 @@ import "../styles/Rocket.css";
 import Stages from "./Stages";
 
 const Rocket = ({ rocket, setRocket }) => {
-  let stages = [];
-  if (!rocket.stages) {
-    stages = [];
+  if (rocket.stages.length !== rocket.totalStages) {
+    return (
+      <div>
+        <h2 className={"rocketName"}>{rocket.name}</h2>
+        <Stages
+          stages={rocket.stages || stages}
+          setStages={(stages) => setRocket({ ...rocket, stages })}
+        />
+        <p>Status: {rocket.stages.length === rocket.totalStages ? "Complete!" : "Incomplete!"}</p>
+      </div>
+    );  
   }
-  return (
-    <div>
-      <h2 className={"rocketName"}>{rocket.name}</h2>
-      <Stages
-        stages={rocket.stages || stages}
-        setStages={(stages) => setRocket({ ...rocket, stages })}
-      />
-    </div>
-  );
+  return <p>UNDER CONSTRUCTION!</p>
 };
 
 export default Rocket;
