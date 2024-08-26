a1ce5acf6678f97f9f5c78bdda4813927c91dbf2
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-08-26 00:11:50 -0400

    feat: add footer component

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,16 @@
+import './styles/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className={"footer"}>
+            <p>
+                © 2024 B.L. Yadecha. &nbsp;
+                <a href="https://github.com/BruhLemma-Yadecha/rocketmancer-web">
+                    GitHub Page
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
