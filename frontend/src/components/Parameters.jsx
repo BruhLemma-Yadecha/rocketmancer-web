86066677a625f44dc6aeeff751757a9d03eccd98
BruhLemma-Yadecha <98387741+BruhLemma-Yadecha@users.noreply.github.com>
2024-08-25 20:39:10 -0400

    feat: add default configuration to be loaded on first page load

--- frontend/src/components/Parameters.jsx
+++ frontend/src/components/Parameters.jsx
@@ -1,24 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios, { Axios } from 'axios';
 
 const Parameters = () => {
-    const [stages, setStages] = useState(1);
-    const [totalDeltaV, setTotalDeltaV] = useState(0);
     const heading = ["Stage", "Specific Impulse", "Propellant Mass Fraction"];
+
+    const [config, setConfig] = useState({
+        name: "", totalStages: 1, totalDeltaV: 0, stages: []
+    })
+
+    useEffect(() => {
+        axios.get("/public/billyJean.json").then((response) => {
+            setConfig(response.data);
+        });
+    }, []);
+
+    const setName = (name) => {
+        setConfig({ ...config, name });
+    }
+
+    const setStages = (totalStages) => {
+        setConfig({ ...config, totalStages });
+    }
+
+    const setTotalDeltaV = (totalDeltaV) => {
+        setConfig({ ...config, totalDeltaV });
+    }
+
     return (
         <div>
             <h1>Parameters</h1>
-            <label>Stages: </label>
-            <input
-                type = "number"
-                value = {stages}
-                onChange = {(e) => setStages(Math.max(1, e.target.value))}
-            /> <br />
-            <label>Total Delta-V:</label>
-            <input
-                type = "number"
-                value = {totalDeltaV}
-                onChange = {(e) => setTotalDeltaV(Math.max(0, e.target.value))}
-            />
+            <label>Name: </label>
+            <input type="text" value={config.name} onChange={(e) => setName(e.target.value)} /> <br />
+            <label>Total Stages: </label>
+            <input type="number" value={config.totalStages} onChange={(e) => setStages(e.target.value)} />  <br />
+            <label>Total Delta-V: </label>
+            <input type="number" value={config.totalDeltaV} onChange={(e) => setTotalDeltaV(e.target.value)} />  <br />
         </div>
     )
 }
